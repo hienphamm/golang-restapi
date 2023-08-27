802083830005e9a3e30a24f4987f42651895b1eb
hienphamm <hien.phamminh2001@gmail.com>
2023-08-27 23:23:08 +0700

    Update schema

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -8,13 +8,13 @@ CREATE TABLE "accounts" (
 
 CREATE TABLE "entries" (
                            "id" bigserial PRIMARY KEY,
-                           "acoount_id" bigint NOT NULL,
+                           "account_id" bigint NOT NULL,
                            "amount" bigint NOT NULL,
                            "created_at" timestamp NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "transfers" (
-                             "id" integer PRIMARY KEY,
+                             "id" bigserial PRIMARY KEY,
                              "from_account_id" bigint NOT NULL,
                              "to_account_id" bigint NOT NULL,
                              "amount" bigint NOT NULL,
@@ -23,7 +23,7 @@ CREATE TABLE "transfers" (
 
 CREATE INDEX ON "accounts" ("owner");
 
-CREATE INDEX ON "entries" ("acoount_id");
+CREATE INDEX ON "entries" ("account_id");
 
 CREATE INDEX ON "transfers" ("from_account_id");
 
@@ -35,7 +35,7 @@ COMMENT ON COLUMN "entries"."amount" IS 'can be negative or positive';
 
 COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
 
-ALTER TABLE "entries" ADD FOREIGN KEY ("acoount_id") REFERENCES "accounts" ("id");
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
 
 ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
 
