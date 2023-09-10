bd15c468cffef73c2ee8a250b4af5330e810914b
hienphamm <hien.phamminh2001@gmail.com>
2023-09-10 22:05:43 +0700

    Implement login API

--- api/main_test.go
+++ api/main_test.go
@@ -2,9 +2,25 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/hienphamm/simplebank/db/sqlc"
+	"github.com/hienphamm/simplebank/util"
+	"github.com/stretchr/testify/require"
 	"os"
 	"testing"
+	"time"
 )
+
+func newTestServer(t *testing.T, store sqlc.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+
+	return server
+}
 
 func TestMain(t *testing.M) {
 	gin.SetMode(gin.TestMode)
